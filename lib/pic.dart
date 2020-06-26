e6e8c467da14b142bdce14950b1f451376a7844c
Mehmet Dogukan Sevinc <nextandbackacademy@gmail.com>
2020-06-26 18:32:08 +0300

    pix

---
+++ lib/pic.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class pic extends StatefulWidget {
+  @override
+  _picState createState() => _picState();
+}
+
+class _picState extends State<pic> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
